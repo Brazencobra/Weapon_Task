cbd5369e8df64d015a5b17e30341bf547cac2166
User <elxan.memmedli2003@gmail.com>
2024-01-24 02:43:43 +0400

    First commit

---
+++ WeaponTask/Models/Weapon.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace WeaponTask.Models
+{
+    internal class Weapon
+    {
+        private int _bulletCapacity;
+        private int _bulletNumber;
+        private int _endSecond;
+        private string _shootMood = "tekli";
+
+        public int BulletCapacity 
+        {
+            get => _bulletCapacity;
+            set
+            {
+                if (value>0 && value <= 50)
+                {
+                    _bulletCapacity = value;
+                }
+            }
+        }
+        public int BulletNumber 
+        {
+            get => _bulletNumber;
+            set
+            {
+                if (value<=_bulletCapacity && value>= 0)
+                {
+                    _bulletNumber = value;
+                }
+            }
+        }
+        public int EndSecond 
+        {
+            get => _endSecond;
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _endSecond = value;
+                }
+            } 
+        }
+        public string ShootMode 
+        {
+            get => _shootMood;
+            set
+            {
+                if (value == "tek" || value == "avto")
+                {
+                    _shootMood = value;
+                }
+            }
+        }
+
+        public Weapon(int bulletcapacity,int bulletnumber,int endsecond,string shootmood)
+        {
+            BulletCapacity = bulletcapacity;
+            BulletNumber = bulletnumber;
+            EndSecond = endsecond;
+            ShootMode = shootmood;
+        }
+
+        public int Shoot()
+        {
+            if (BulletNumber > 0)
+            {
+                BulletNumber--;
+                return BulletNumber;
+            }
+            return 0;
+        }
+        
+        public int Fire()
+        {
+            if (ShootMode == "tek")
+            {
+                int lastnumber = BulletNumber / 2;
+                BulletNumber = 0;
+                return lastnumber;
+            }
+            int lastnumber1 = BulletNumber / 3;
+            BulletNumber = 0;
+            return lastnumber1;
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - BulletNumber;
+        }
+
+        public void Reload()
+        {
+            if (BulletNumber == BulletCapacity)Console.WriteLine("Sizin daraginiz doludur Reload olunmadi");
+            else
+            {
+                int needammo = 0;
+                needammo = BulletCapacity - BulletNumber;
+                Console.WriteLine("Sizin daraga " + needammo + " gulle elave edilerek " + BulletCapacity + " oldu");
+                BulletNumber += needammo; 
+
+            }
+        }
+
+        public void ChangeFireMode()
+        {
+            if (ShootMode == "tek") ShootMode = "avto";
+            else
+            {
+                ShootMode = "tek";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Silah maqazin tutumu - {BulletCapacity}\nMaqazinde olan gulle sayi - {BulletNumber}\nGullelerin bitme muddeti - {EndSecond}\nSilahin atis modu - {ShootMode}";
+        }
+    }
+}
---
+++ WeaponTask/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using WeaponTask.Models;
+
+namespace WeaponTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(30, 15, 5, "tek");
+            bool respond = true;
+            do
+            {
+                Console.WriteLine(" - - - - - - - - - - - - - ");
+                Console.WriteLine("0 - İnformasiya almaq üçün\r\n1 - Shoot metodu üçün\r\n2 - Fire metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün");
+                Console.WriteLine(" - - - - - - - - - - - - - ");
+                byte input = Convert.ToByte(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 0:
+                        Console.WriteLine(weapon.ToString());
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        Console.WriteLine($"Bir defe ates edildi qalan gulle sayi : {weapon.BulletNumber}");
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        Console.WriteLine("Butun gulleler ateslendi Reload etmek isteyirsinizse 4 daxil edin");
+                        break;
+                    case 3:
+                        Console.WriteLine("Sizin daraginizin tam dolmasi ucun lazim olan gulle miqdari : " + weapon.GetRemainBulletCount());
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        Console.WriteLine("Indiki silah modunuz : " + weapon.ShootMode);
+                        weapon.ChangeFireMode();
+                        Console.WriteLine("Silah modunuz deyisdirildi : " + weapon.ShootMode);
+                        break;
+                    case 6:
+                        respond = false;
+                        break;
+                    default:
+                        Console.WriteLine("Yanlis daxil edilme");
+                        break;
+                }
+            } while (respond);
+        }
+    }
+}
